09bac39f82d2c235b0d4c50d7d91da995f2b5db7
Natan18s <natanssampaio18@gmail.com>
2026-05-23 08:37:14 -0300

    css js

---
+++ frontend/js/script.js
@@ -0,0 +1,15 @@
+// Seleciona o primeiro botão encontrado na página
+const botao = document.querySelector("button");
+
+/*
+  Adiciona um evento de clique ao botão
+
+  Quando o usuário clicar:
+  executa a função
+*/
+botao.addEventListener("click", () => {
+
+  // Mostra uma janela de alerta
+  alert("Área de agendamento");
+
+});
